c194681371529871a1532c3285329a0f78d9831b
Kira Corbett <kiracorbett00@gmail.com>
2020-06-09 09:40:27 -0700

    check for reset method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,12 @@ describe 'A Tic Tac Toe board' do
   	Board.new
   end
 
+  it '#reset' do
+  	board = Board.new
+  	expect(board.reset)
+  	expect(board.reset).to be_truthy
+  end
+
   it '#empty' do
   	board = Board.new
   	expect(board.empty?).to be_truthy
