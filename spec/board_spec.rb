470b2f9e38507de0078ada25e15dcdc25476c548
Kira Corbett <kiracorbett00@gmail.com>
2020-06-09 09:49:09 -0700

    attempted full test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,6 +8,11 @@ describe 'A Tic Tac Toe board' do
   	@board
   end
 
+  it '#place' do
+  	@board
+  	expect(@board.empty?).to be_truthy
+  end
+
   it '#reset' do
   	expect(@board.reset)
   	expect(@board.reset).to be_truthy
@@ -16,5 +21,11 @@ describe 'A Tic Tac Toe board' do
   it '#empty' do
   	expect(@board.empty?).to be_truthy
   end
+
+  # it '#full' do
+  # 	@board
+  # 	@board = @board.full?
+  # 	expect(@board.empty?).to be_falsey
+  # end
 
 end
